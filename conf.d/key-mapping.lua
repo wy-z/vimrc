210d83c8878258f2da5fa60ea181a5004c8b7af1
weiyang <weiyang.ones@gmail.com>
2023-12-06 15:30:05 +0800

    Refine key mapping
    Signed-off-by: weiyang <weiyang.ones@gmail.com>

--- conf.d/key-mapping.lua
+++ conf.d/key-mapping.lua
@@ -13,14 +13,12 @@ end
 -- which keys
 for k, v in pairs({
 	["p"] = { "<cmd>lua require('fzf-lua').files()<cr>", "Serach files" },
-	["pp"] = { "<cmd>lua require('fzf-lua').git_files()<cr>", "Search `git ls-files`" },
+	["f"] = { "<cmd>lua require('fzf-lua').git_files()<cr>", "Search `git ls-files`" },
 	["j"] = { "<cmd>lua require('fzf-lua').lsp_document_symbols()<cr>", "Search symbols" },
 	["s"] = { "<cmd>lua require('fzf-lua').lgrep_curbuf()<cr>", "Search current buffer" },
 	["S"] = { "<cmd>lua require('spectre').open()<CR>", "Open Spectre" },
 	["<leader>"] = { "<cmd>lua require('fzf-lua').commands()<cr>", "Lists commands" },
 	["/"] = { "<cmd>lua require('fzf-lua').live_grep()<cr>", "Search text, respects .gitignore" },
-	["tt"] = { "<cmd>TroubleToggle<cr>", "Toggle trouble" },
-	["tf"] = { "<cmd>NvimTreeToggle<cr>", "Toggle nvim-tree" },
 }) do
 	lvim.builtin.which_key.mappings[k] = v
 end
