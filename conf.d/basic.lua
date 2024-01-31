7142b612aa4e959b611d5254503cefda6f91a8fe
weiyang <weiyang.ones@gmail.com>
2024-01-31 16:51:16 +0800

    Add tpope/vim-sleuth
    Signed-off-by: weiyang <weiyang.ones@gmail.com>

--- conf.d/basic.lua
+++ conf.d/basic.lua
@@ -49,6 +49,8 @@ for _, v in ipairs({
 	},
 	-- enable repeating supported plugin maps with "."
 	"tpope/vim-repeat",
+	-- Heuristically set buffer options
+	"tpope/vim-sleuth",
 	-- Vim plugin that allows you to visually select increasingly larger regions of text using the same key combination.
 	"terryma/vim-expand-region",
 	-- Make Vim persist editing state without fuss
