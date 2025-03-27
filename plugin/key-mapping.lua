b7876c0147ccbd31bcabbecbc2c5985efeffb5c8
weiyang <weiyang.ones@gmail.com>
2025-03-27 23:33:10 +0800

    fix(keymap): update leader-s mapping to use fl instead of f/
    Signed-off-by: weiyang <weiyang.ones@gmail.com>

--- plugin/key-mapping.lua
+++ plugin/key-mapping.lua
@@ -4,7 +4,7 @@ vim.cmd('map cp "+y')
 
 -- alias key maps
 vim.keymap.set("n", "<leader>p", "<leader>ff", { remap = true })
-vim.keymap.set("n", "<leader>s", "<leader>f/", { remap = true })
+vim.keymap.set("n", "<leader>s", "<leader>fl", { remap = true })
 vim.keymap.set("n", "<leader>j", "<leader>ls", { remap = true })
 vim.keymap.set("n", "<leader>/", "<leader>fW", { remap = true })
 vim.keymap.set("n", "<leader><leader>", "<leader>fC", { remap = true })
